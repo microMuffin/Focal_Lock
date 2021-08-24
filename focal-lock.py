e8fafd67ad8f6e6204ff8717a7c61ec7bbaf5690
AnsonSavage <ansonsavage@gmail.com>
2021-08-24 10:44:36 -0600

    GPL
    Added GPL License Block

--- focal-lock.py
+++ focal-lock.py
@@ -1,3 +1,17 @@
+#     GPL License Block
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 bl_info = {
     "name": "Focal Lock",
     "description": "Locks object in a camera's plane of focus",
@@ -11,7 +25,6 @@ bl_info = {
     "category": "Camera",
     }
 
-#UPDATE EVERYTHING SO THAT IT WILL WORK FOR ANY CAMERA (AND IF YOU CAN, SO THAT YOU CAN HAVE MULTIPLE CAMERAS)
 # LIBRARIES
 ############################################################################
 from mathutils import Vector
