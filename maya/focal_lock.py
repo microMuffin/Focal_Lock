84f9798812a08b73178168266dd0bd2e198c017e
AnsonSavage <ansonsavage@gmail.com>
2023-06-02 20:20:24 -0600

    Removed some of the unnecessary code

--- maya/focal_lock.py
+++ maya/focal_lock.py
@@ -1,6 +1,6 @@
 import maya.cmds as cmds
 
-def maintainDistance(cam, obj, distance, focalLengthRatio):
+def maintainFocalLengthRatio(cam, obj, focalLengthRatio):
     # get current position of obj
     objPos = cmds.xform(obj, q=True, t=True, ws=True)
     
@@ -17,32 +17,16 @@ def maintainDistance(cam, obj, distance, focalLengthRatio):
     newFocalLength = actualDist * focalLengthRatio
     cmds.setAttr(cam + '.focalLength', newFocalLength)
 
-    # normalise vector
-    vec = [vec[0]/actualDist, vec[1]/actualDist, vec[2]/actualDist]
-
-    # multiply vector by desired distance
-    vec = [vec[0]*distance, vec[1]*distance, vec[2]*distance]
-
-    # position cam at objPos - vec
-    newPos = [objPos[0] - vec[0], objPos[1] - vec[1], objPos[2] - vec[2]]
-    cmds.xform(cam, t=newPos, ws=True)
-
 
 # initialize distance and focal length ratio
-distance = 35  # typical default focal length
 focalLengthRatio = 1.0
 
 def maintainFocalLength(*args):
     """ Callback function for the button. """
-    global distance, focalLengthRatio
+    global focalLengthRatio
     cam = cmds.optionMenu(cameraMenu, query=True, value=True)
     obj = cmds.optionMenu(objectMenu, query=True, value=True)
-    maintainDistance(cam, obj, distance, focalLengthRatio)
-
-def onDistanceChanged(*args):
-    """ Callback function for the distance field. """
-    global distance
-    distance = cmds.floatField(distanceField, query=True, value=True)
+    maintainFocalLengthRatio(cam, obj, focalLengthRatio)
 
 def onFocalLengthRatioChanged(*args):
     """ Callback function for the focal length ratio field. """
@@ -65,9 +49,6 @@ objects = cmds.ls(dag=True, leaf=True, noIntermediate=True, shapes=True)
 objects = [object for object in objects if object not in cmds.ls(cameras=True)] # Exclude the cameras from this list
 for obj in objects:
     cmds.menuItem(label=obj)
-
-cmds.text(label='Distance to maintain:')
-distanceField = cmds.floatField(minValue=0.0, value=distance, changeCommand=onDistanceChanged)
 
 cmds.text(label='Focal Length Ratio:')
 focalLengthRatioField = cmds.floatField(minValue=0.0, value=focalLengthRatio, changeCommand=onFocalLengthRatioChanged)
