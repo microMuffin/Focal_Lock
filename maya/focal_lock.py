9b20c3d682a8ddd6187fff2da702e56e50a139aa
AnsonSavage <ansonsavage@gmail.com>
2023-06-02 20:47:27 -0600

    Using the transform nodes now...

--- maya/focal_lock.py
+++ maya/focal_lock.py
@@ -1,9 +1,42 @@
 import maya.cmds as cmds
 
-def maintainFocalLengthRatio(cam, obj, focalLengthRatio):
+# Maintain a script job ID
+scriptJobId = None
+focalLengthRatio = 1.0
+
+def computeFocalLengthRatio(cam, obj):
+    if not cmds.objExists(cam) or not cmds.objExists(obj):
+        print("Camera or object does not exist.")
+        return
+
+    print(obj)
     # get current position of obj
     objPos = cmds.xform(obj, q=True, t=True, ws=True)
-    
+
+    # get current position of camera
+    camPos = cmds.xform(cam, q=True, t=True, ws=True)
+
+    # calculate vector from cam to obj
+    vec = [objPos[0] - camPos[0], objPos[1] - camPos[1], objPos[2] - camPos[2]]
+
+    # calculate actual distance
+    actualDist = (vec[0]**2 + vec[1]**2 + vec[2]**2)**0.5
+
+    # get the current camera's focal length
+    currentFocalLength = cmds.getAttr(cam + '.focalLength')
+
+    # calculate the focal length ratio
+    global focalLengthRatio
+    focalLengthRatio = currentFocalLength / actualDist
+
+def maintainFocalLengthRatio(cam, obj):
+    if not cmds.objExists(cam) or not cmds.objExists(obj):
+        print("Camera or object does not exist.")
+        return
+
+    # get current position of obj
+    objPos = cmds.xform(obj, q=True, t=True, ws=True)
+
     # get current position of camera
     camPos = cmds.xform(cam, q=True, t=True, ws=True)
 
@@ -17,44 +50,84 @@ def maintainFocalLengthRatio(cam, obj, focalLengthRatio):
     newFocalLength = actualDist * focalLengthRatio
     cmds.setAttr(cam + '.focalLength', newFocalLength)
 
-
-# initialize distance and focal length ratio
-focalLengthRatio = 1.0
-
-def maintainFocalLength(*args):
-    """ Callback function for the button. """
-    global focalLengthRatio
+def onObjectChanged(*args):
+    """ Callback function for the object dropdown menu change. """
+    global scriptJobId
     cam = cmds.optionMenu(cameraMenu, query=True, value=True)
     obj = cmds.optionMenu(objectMenu, query=True, value=True)
-    maintainFocalLengthRatio(cam, obj, focalLengthRatio)
+    computeFocalLengthRatio(cam, obj)
+    if scriptJobId:
+        cmds.scriptJob(kill=scriptJobId, force=True)
+        scriptJobId = cmds.scriptJob(e=("idle", lambda: maintainFocalLengthRatio(cam, obj)), protected=True)
 
-def onFocalLengthRatioChanged(*args):
-    """ Callback function for the focal length ratio field. """
-    global focalLengthRatio
-    focalLengthRatio = cmds.floatField(focalLengthRatioField, query=True, value=True)
+# def onEnableLockChanged(*args):
+#     """ Callback function for the lock checkbox. """
+#     global lockId, focalLengthRatio
 
-# create the window
+    if cmds.checkBox(lockCheckBox, query=True, value=True):
+        cam = cmds.optionMenu(cameraMenu, query=True, value=True)
+        obj = cmds.optionMenu(objectMenu, query=True, value=True)
+
+        # check if cam or obj is None or invalid
+        if not cam or not obj or not cmds.objExists(cam) or not cmds.objExists(obj):
+            cmds.warning("Invalid camera or object selected.")
+            cmds.checkBox(lockCheckBox, edit=True, value=False)  # uncheck the checkbox
+            return
+        
+        # further operations with cam and obj...
+
+def onFocalLockChanged(enabled):
+    """ Callback function for the focal lock checkbox change. """
+    global scriptJobId
+    if enabled:
+        cam = cmds.optionMenu(cameraMenu, query=True, value=True)
+        obj = cmds.optionMenu(objectMenu, query=True, value=True)
+        if not cam or not obj or not cmds.objExists(cam) or not cmds.objExists(obj):
+            cmds.warning("Invalid camera or object selected.")
+            cmds.checkBox(focalLockCheckbox, edit=True, value=False)  # uncheck the checkbox
+            return
+        computeFocalLengthRatio(cam, obj)
+        scriptJobId = cmds.scriptJob(e=("idle", lambda: maintainFocalLengthRatio(cam, obj)), protected=True)
+    else:
+        # Kill the script job
+        if scriptJobId:
+            cmds.scriptJob(kill=scriptJobId, force=True)
+            scriptJobId = None
+
+def onWindowClose(killOnClose=True):
+    """ Callback function for window close. """
+    global scriptJobId
+    if scriptJobId and killOnClose:
+        # Kill the script job
+        cmds.scriptJob(kill=scriptJobId, force=True)
+        scriptJobId = None
+
+# Create the window
 window = cmds.window(title="Focal Lock", widthHeight=(200, 100))
 cmds.columnLayout(adjustableColumn=True)
 
-# create the user interface elements
+cameraShapeList = cmds.ls(cameras=True)
+cameraList = cmds.listRelatives(cameraShapeList, parent=True)
+
+# Create the user interface elements
 cmds.text(label='Camera:')
-cameraMenu = cmds.optionMenu()
-for cam in cmds.ls(cameras=True):
+cameraMenu = cmds.optionMenu(changeCommand=onObjectChanged)
+for cam in cameraList:
     cmds.menuItem(label=cam)
 
 cmds.text(label='Target Object:')
-objectMenu = cmds.optionMenu()
-objects = cmds.ls(dag=True, leaf=True, noIntermediate=True, shapes=True)
-objects = [object for object in objects if object not in cmds.ls(cameras=True)] # Exclude the cameras from this list
-for obj in objects:
-    cmds.menuItem(label=obj)
+objectMenu = cmds.optionMenu(changeCommand=onObjectChanged)
+shapeNodes = cmds.ls(dag=True, leaf=True, noIntermediate=True, shapes=True)
+transformNodes = cmds.listRelatives(shapeNodes, parent=True, fullPath=True) # Get parent transform nodes
+transformNodes = [node for node in transformNodes if node not in cameraList] # Exclude camera transform nodes from this list
+for node in transformNodes:
+    cmds.menuItem(label=node)
 
-cmds.text(label='Focal Length Ratio:')
-focalLengthRatioField = cmds.floatField(minValue=0.0, value=focalLengthRatio, changeCommand=onFocalLengthRatioChanged)
+focalLockCheckbox = cmds.checkBox(label='Lock Focal Length', changeCommand=onFocalLockChanged)
+killOnCloseCheckbox = cmds.checkBox(label='Kill on Close', value=True)
 
-cmds.button(label="Lock Focal Length", command=maintainFocalLength)
-
+# Show the window
 cmds.showWindow(window)
 
-# Test
+# Create a script job that triggers when the window is deleted
+cmds.scriptJob(uiDeleted=(window, onWindowClose))
