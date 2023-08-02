8040d591a8bbd7f818becad02e91faf539acaf76
Anson Savage <ansonsavage@gmail.com>
2023-08-01 20:55:33 -0600

    It's working!!

--- maya/focal_lock.py
+++ maya/focal_lock.py
@@ -1,16 +1,63 @@
 import maya.cmds as cmds
+import math
+
+focal_length_ratio = None
+
+def dotProduct(v1, v2):
+    return v1[0]*v2[0] + v1[1]*v2[1] + v1[2]*v2[2]
+
+def computeForwardVector(rotation):
+    # Convert the rotation to radians
+    rotation = [math.radians(x) for x in rotation]
+
+    # Calculate the forward direction vector based on Maya's default orientation
+    forwardVector = [
+        -math.sin(rotation[1])*math.cos(rotation[0]), 
+        math.sin(rotation[0]), 
+        -math.cos(rotation[1])*math.cos(rotation[0])
+    ]
+
+    return forwardVector
+
+def compute_distance_along_camera_forward_vector(camera_name, target_object_name):
+    object_position = cmds.xform(target_object_name, query=True, translation=True, worldSpace=True)
+    camera_position = cmds.xform(camera_name, query=True, translation=True, worldSpace=True)
+    camera_rotation = cmds.xform(camera_name, query=True, rotation=True, worldSpace=True)
+
+    print(f'Object Position: {object_position}')
+    print(f'Camera Position: {camera_position}')
+    print(f'Camera Rotation: {camera_rotation}')
+
+    camera_forward_vector = computeForwardVector(camera_rotation)
+
+    # Calculate the vector from the camera to the object
+    camera_to_object_vector = [object_position[0] - camera_position[0], object_position[1] - camera_position[1], object_position[2] - camera_position[2]]
+
+    # Calculate the distance along the camera's forward vector
+    distance = dotProduct(camera_to_object_vector, camera_forward_vector)
+
+    return distance
 
 def add_focal_length_expression(camera_name, target_object_name):
+    global focal_length_ratio
+
     # Get the transform node associated with the camera
-    transform_node = cmds.listRelatives(camera_name, parent=True, fullPath=True)[0]
+    camera_transform_node = cmds.listRelatives(camera_name, parent=True, fullPath=True)[0]
+
+    initial_focal_length = cmds.getAttr(camera_name + '.focalLength')
+    initial_distance = compute_distance_along_camera_forward_vector(camera_transform_node, target_object_name)
+    focal_length_ratio = initial_focal_length / initial_distance
 
     expression_name = 'DistanceToFocalLengthExpression'
 
     expression_string = f"""
-    float $distance = pow(pow(({transform_node}.translateX - {target_object_name}.translateX), 2.0) + 
-                         pow(({transform_node}.translateY - {target_object_name}.translateY), 2.0) + 
-                         pow(({transform_node}.translateZ - {target_object_name}.translateZ), 2.0), 0.5);
-    {camera_name}.focalLength = $distance;
+    float $distance = pow(pow(({camera_transform_node}.translateX - {target_object_name}.translateX), 2.0) + 
+                         pow(({camera_transform_node}.translateY - {target_object_name}.translateY), 2.0) + 
+                         pow(({camera_transform_node}.translateZ - {target_object_name}.translateZ), 2.0), 0.5);
+
+    // Store initial focal length and distance when the expression is created
+    // Calculate updated focal length based on the initial ratio
+    {camera_name}.focalLength = $distance * {focal_length_ratio};
     """
 
     # Check if the expression already exists and delete it if it does
